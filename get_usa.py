f9777310a019b4a009fb4066c8307fe6dfcb623a
Jason Kao <jasonkao85@gmail.com>
2018-01-20 19:36:34 -0500

    writi

--- get_usa.py
+++ get_usa.py
@@ -4,4 +4,9 @@ c = i.GeoJsonCountryGridCreater('^', 'districts/cds/2016/')
 # print i.image_string(c.get_country_grid_with_more_info())
 s = c.get_country_grid_with_more_info()
 
-print s.copy_array()
+def strip_zeroes(matrix):
+    return [row for row in matrix if any(map(bool, row)) > 0]
+
+a = strip_zeroes(s.copy_array())
+with open('usa.json', 'w') as f:
+    json.dump(a, f)
